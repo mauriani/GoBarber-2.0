be65947832df7840fa7b981d63a9743d371117e0
mauriani <maurianim@gmail.com>
2021-04-26 15:29:11 -0300

    Criando o primeiro teste

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,30 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import CreateAppointmentServices from './CreateAppointmentService';
+
+// Categorizando os nossos testes
+// it - significa isso
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    // fakeappoint onde os dados são salvos
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    // CRIANDO O SERVICE
+    // meu create appoint espera dois parametros (create e findByEmail),
+    // passamos a fake api onde temos essas funcionalidades.
+    const createAppointment = new CreateAppointmentServices(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    // espero que meu appointment tenha um id
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {});
+});
