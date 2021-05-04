9217ea51f208e521e403cc87e7b885335dfc14b5
mauriani <maurianim@gmail.com>
2021-05-04 08:24:03 -0300

    Refatorando o código

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -34,6 +34,15 @@ describe('UpadateProfile', () => {
     expect(updateUser.name).toBe('John Trê');
     expect(updateUser.email).toBe('johntre@example.com');
   });
+  it('shoulnot d be able to update the profile fron non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'Test',
+        email: 'testeemail@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
