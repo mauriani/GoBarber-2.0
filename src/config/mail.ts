f8b3eadbda47d4d2186d237385c2b6e85d0c5619
mauriani <maurianim@gmail.com>
2021-05-12 10:56:07 -0300

    emails pelo amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'maurianim@gmail.com',
+      name: 'Mauriani Maciel',
+    },
+  },
+} as IMailConfig;
