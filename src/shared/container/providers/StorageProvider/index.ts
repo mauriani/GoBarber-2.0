57f6410de5ddd0b442b2db5823216eae73d4782f
mauriani <maurianim@gmail.com>
2021-05-12 11:17:51 -0300

    organizando os containers

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,15 @@
+// mapeamento do meu mmodels
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers.disk,
+);
