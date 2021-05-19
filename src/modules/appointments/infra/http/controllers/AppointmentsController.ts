ea8254c1b3882a724e1cc47bc6a47dd8e27cb5b9
mauriani <maurianim@gmail.com>
2021-05-19 09:06:59 -0300

    alterações em rotas de listagem de dias (rota tipo index tem que ter dados passados através do nosso query params

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express';
-
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '../../../services/CreateAppointmentService';
